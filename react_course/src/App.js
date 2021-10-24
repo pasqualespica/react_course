b55cf67f5efe81bc03e275fe2aed7e9942c8ca04
pasqualespica <pasqualespica@gmail.com>
2021-10-24 20:08:10 +0200

    add a component

--- react_course/src/App.js
+++ react_course/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
